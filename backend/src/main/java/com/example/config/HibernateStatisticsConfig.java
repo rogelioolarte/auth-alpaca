f7617a5bf423387d3359918009789105753ac391
candidate-mNiNDlhMDY <gasper_lf@hotmail.com>
2025-02-13 23:43:12 +0100

    FEAT: added configuration to run test

--- backend/src/main/java/com/example/config/HibernateStatisticsConfig.java
+++ backend/src/main/java/com/example/config/HibernateStatisticsConfig.java
@@ -8,13 +8,4 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class HibernateStatisticsConfig {
 
-    private final EntityManagerFactory entityManagerFactory;
-
-//    @PostConstruct
-//    public void enableStatistics() {
-//        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
-//        Statistics stats = sessionFactory.getStatistics();
-//        stats.setStatisticsEnabled(true);
-//        System.out.println("Hibernate Statistics enabled");
-//    }
 }
